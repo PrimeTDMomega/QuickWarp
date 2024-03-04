c19ca56df0c7d6557101319bd9fccff1d6df0358
prime <105910256+PrimeTDMomega@users.noreply.github.com>
2024-03-04 13:13:37 +0530

    reworked plugin

--- src/main/java/me/prime/quickwarp/QuickWarp.java
+++ src/main/java/me/prime/quickwarp/QuickWarp.java
@@ -1,50 +1,66 @@
-
-// Written by primetdm@discord 4/03/2024
-
-package me.prime.quickwarp;
+// WK owns u and all
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
+public class QuickWarp extends JavaPlugin implements CommandExecutor {
 
-public class QuickWarp implements CommandExecutor {
+    private FileConfiguration config;
+    private Map<String, Location> warpLocations;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        config = getConfig();
+        warpLocations = new HashMap<>();
+        warpLocations.put("overworld", new Location(Bukkit.getWorld("world"), 0, 70, 0));
+        warpLocations.put("nether", new Location(Bukkit.getWorld("DIM-1"), 0, 70, 0));
+        warpLocations.put("end", new Location(Bukkit.getWorld("DIM1"), 0, 70, 0));
+        getCommand("qw").setExecutor(this);
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("Only players can execute this command.");
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can use this command.");
             return true;
         }
 
-        World targetWorld;
-        switch (command.getName().toLowerCase()) {
-            case "end":
-                targetWorld = Bukkit.getWorld("world_the_end");
-                break;
-            case "overworld":
-                targetWorld = Bukkit.getWorld("world");
-                break;
-            case "nether":
-                targetWorld = Bukkit.getWorld("world_nether");
-                break;
-            default:
-                player.sendMessage("Invalid command. Use /end, /overworld or /nether.");
-                return true;
-        }
+        Player player = (Player) sender;
 
-        if (player.getWorld().equals(targetWorld)) {
-            player.sendMessage("You cannot warp to the dimension you are in...");
+        if (args.length == 0) {
+            player.sendMessage(ChatColor.RED + "Usage: /qw <overworld|nether|end>");
             return true;
         }
 
-        Location targetLocation = new Location(targetWorld, 0, 64, 0);
-        player.teleport(targetLocation);
+        String dimension = args[0].toLowerCase();
+
+        if (!warpLocations.containsKey(dimension)) {
+            player.sendMessage(ChatColor.RED + "Invalid dimension. Use overworld, nether, or end.");
+            return true;
+        }
+
+        Location warpLocation = warpLocations.get(dimension);
+
+        if (player.getWorld().getEnvironment().equals(warpLocation.getWorld().getEnvironment())) {
+            player.sendMessage(ChatColor.RED + config.getString("errorMSG"));
+            return true;
+        }
+
+        player.teleport(warpLocation);
+        player.sendMessage(ChatColor.GREEN + "Teleported to " + dimension + ".");
         return true;
     }
 }
