e5e2d67884d143597911302e284ae43c607232cb
Prime <shreyaanmisra0@gmail.com>
2024-03-04 15:39:37 +0530

    reimplemented particle effect

--- src/main/java/me/prime/quickwarp/QuickWarp.java
+++ src/main/java/me/prime/quickwarp/QuickWarp.java
@@ -3,30 +3,26 @@ package me.prime.quickwarp;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Particle; // WHY IS THIS GIVING ME AN ERROR
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.prime.quickwarp.effects.Teleport;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
-
 
 public class QuickWarp extends JavaPlugin {
 
     private FileConfiguration config;
-    private Map<UUID, Long> cooldowns;
     private Map<String, Location> warpLocations;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         config = getConfig();
-        cooldowns = new HashMap<>();
         warpLocations = new HashMap<>();
         warpLocations.put("overworld", new Location(Bukkit.getWorld("world"), 0, 70, 0));
         warpLocations.put("nether", new Location(Bukkit.getWorld("DIM-1"), 0, 70, 0));
@@ -62,16 +58,6 @@ public class QuickWarp extends JavaPlugin {
             return true;
         }
 
-        if (cooldowns.containsKey(player.getUniqueId())) {
-            long currentTime = System.currentTimeMillis();
-            long cooldownEnd = cooldowns.get(player.getUniqueId());
-            if (currentTime < cooldownEnd) {
-                long remainingTime = (cooldownEnd - currentTime) / 1000;
-                player.sendMessage(ChatColor.RED + "Please wait " + remainingTime + " more seconds before using this command again.");
-                return true;
-            }
-        }
-
         Location warpLocation = warpLocations.get(dimension);
 
         if (player.getWorld().getEnvironment().equals(warpLocation.getWorld().getEnvironment())) {
@@ -80,18 +66,8 @@ public class QuickWarp extends JavaPlugin {
         }
 
         player.teleport(warpLocation);
+        Teleport.playEffect(player, warpLocation);
         player.sendMessage(ChatColor.GREEN + "Teleported to " + dimension + ".");
-
-        // Spiralling fire particle effect
-        World world = player.getWorld();
-        for (double angle = 0; angle < 360; angle += 5) {
-            double x = warpLocation.getX() + 0.5 * Math.cos(Math.toRadians(angle));
-            double z = warpLocation.getZ() + 0.5 * Math.sin(Math.toRadians(angle));
-            Location particleLocation = new Location(world, x, warpLocation.getY(), z);
-            world.spawnParticle(Particle.`enter code here`FLAME, particleLocation, 1, 0, 0, 0, 0);
-        }
-
-        cooldowns.put(player.getUniqueId(), System.currentTimeMillis() + (config.getLong("cooldown") * 1000));
         return true;
     }
 
