e5f52b4b835bf45e2eb48c4ac2d4cc7ab40fc4a3
Prime <shreyaanmisra0@gmail.com>
2024-03-04 11:42:11 +0530

    i dunno

--- src/main/java/me/prime/quickwarp/QuickWarp.java
+++ src/main/java/me/prime/quickwarp/QuickWarp.java
@@ -1,17 +1,48 @@
+
+// Written by primetdm@discord 4/03/2024
+
 package me.prime.quickwarp;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public final class QuickWarp extends JavaPlugin {
+public class QuickWarp implements CommandExecutor {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only players can execute this command.");
+            return true;
+        }
 
-    }
+        World targetWorld;
+        switch (command.getName().toLowerCase()) {
+            case "end":
+                targetWorld = Bukkit.getWorld("world_the_end");
+                break;
+            case "overworld":
+                targetWorld = Bukkit.getWorld("world");
+                break;
+            case "nether":
+                targetWorld = Bukkit.getWorld("world_nether");
+                break;
+            default:
+                player.sendMessage("Invalid command. Use /end, /overworld or /nether.");
+                return true;
+        }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        if (player.getWorld().equals(targetWorld)) {
+            player.sendMessage("You cannot warp to the dimension you are in...");
+            return true;
+        }
+
+        Location targetLocation = new Location(targetWorld, 0, 64, 0);
+        player.teleport(targetLocation);
+        return true;
     }
 }
