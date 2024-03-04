5e99255f781bfccdd867a1ae6293d8a176bc8835
Prime <shreyaanmisra0@gmail.com>
2024-03-04 11:48:39 +0530

    stuff

--- src/main/java/me/prime/quickwarp/Main.java
+++ src/main/java/me/prime/quickwarp/Main.java
@@ -1,19 +1,14 @@
-
-// Written by primetdm@discord 4/03/2024
-
 package me.prime.quickwarp;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 
-
-
-public class QuickWarp implements CommandExecutor {
+public class QuickWarpMain implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -46,5 +41,17 @@ public class QuickWarp implements CommandExecutor {
         Location targetLocation = new Location(targetWorld, 0, 64, 0);
         player.teleport(targetLocation);
         return true;
+    }
+}
+
+
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getCommand("end").setExecutor(new QuickWarp());
+        getCommand("overworld").setExecutor(new QuickWarp());
+        getCommand("nether").setExecutor(new QuickWarp());
     }
 }
