3d12b69489db5d2e2814af65b8e4f1a5d07880ab
Prime <shreyaanmisra0@gmail.com>
2024-03-04 11:37:54 +0530

    init quickwarp

---
+++ src/main/java/me/prime/quickwarp/QuickWarp.java
@@ -0,0 +1,17 @@
+package me.prime.quickwarp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuickWarp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
